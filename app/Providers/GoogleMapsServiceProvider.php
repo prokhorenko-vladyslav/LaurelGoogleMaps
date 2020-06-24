16f8311c54388dd0ff4b843202739338148f4de9
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-06-24 15:45:03 +0300

    Added config merge and publishe to package provider

--- app/Providers/GoogleMapsServiceProvider.php
+++ app/Providers/GoogleMapsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Laurel\GoogleMaps\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laurel\GoogleMaps\GoogleMaps;
 
 class GoogleMapsServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class GoogleMapsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../../config/laurel_google_maps.php', 'laurel_google_maps');
+        $this->publishes([
+            __DIR__ . '/../../config/laurel_google_maps.php' => config_path('laurel_google_maps.php')
+        ], 'config');
     }
 
     /**
@@ -23,6 +27,6 @@ class GoogleMapsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        GoogleMaps::instance();
     }
 }
