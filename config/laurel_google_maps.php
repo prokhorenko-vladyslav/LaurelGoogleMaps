f867e9b7265e36ad999c055d555c7a83154dbe8a
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-06-24 15:37:59 +0300

    Added config file

---
+++ config/laurel_google_maps.php
@@ -0,0 +1,37 @@
+<?php
+    return [
+        'google_maps_api_token' => 'GOOGLE_MAPS_API_TOKEN',
+        'google_maps_api_endpoint' => 'https://maps.googleapis.com/maps/api/',
+        'countries' => [
+            'table_name' => 'NAME_OF_THE_TABLE_WITH_COUNTRIES',
+            'fields' => [
+                'google_id' => '',
+                'name' => '',
+                'slug' => ''
+            ]
+        ],
+        'regions' => [
+            'table_name' => 'NAME_OF_THE_TABLE_WITH_REGIONS',
+            'fields' => [
+                'google_id' => '',
+                'name' => '',
+                'slug' => ''
+            ]
+        ],
+        'cities' => [
+            'table_name' => 'NAME_OF_THE_TABLE_WITH_CITIES',
+            'fields' => [
+                'google_id' => '',
+                'name' => '',
+                'slug' => ''
+            ]
+        ],
+        'postal_codes' => [
+            'table_name' => 'NAME_OF_THE_TABLE_WITH_POSTAL_CODES',
+            'fields' => [
+                'google_id' => '',
+                'name' => '',
+                'slug' => ''
+            ]
+        ],
+    ];
