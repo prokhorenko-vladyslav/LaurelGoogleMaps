02d8f5fa4737c2925bf55519c33f5aa9b898528c
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-06-25 10:17:15 +0300

    Added comments to code

--- GoogleMaps.php
+++ GoogleMaps.php
@@ -2,31 +2,94 @@
 
 namespace Laurel\GoogleMaps;
 
-
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Str;
 use Laurel\GoogleMaps\App\Traits\Remotable;
 use Mockery\Exception;
 
+/**
+ * Class for fetching and saving predictions from GoogleMaps API
+ *
+ * Class GoogleMaps
+ * @package Laurel\GoogleMaps
+ */
 class GoogleMaps
 {
     use Remotable;
 
+    /**
+     * Instance of package entity
+     *
+     * @var
+     */
     protected static $instance;
 
+    /**
+     * Need to save predictions or not
+     *
+     * @var bool
+     */
     protected $savePredictions;
 
+    /**
+     * Class name of a country model
+     *
+     * @var
+     */
     protected $countryModelClass;
+
+    /**
+     * Class name of a region model
+     *
+     * @var
+     */
     protected $regionModelClass;
+
+    /**
+     * Class name of a city model
+     *
+     * @var
+     */
     protected $cityModelClass;
+
+    /**
+     * Class name of a postal code model
+     *
+     * @var
+     */
     protected $postalCodeModelClass;
 
+    /**
+     * City model
+     *
+     * @var
+     */
     protected $cityModel;
+
+    /**
+     * Region model
+     *
+     * @var
+     */
     protected $regionModel;
+
+    /**
+     * Country model
+     *
+     * @var
+     */
     protected $countryModel;
+
+    /**
+     * Model of a postal code
+     *
+     * @var
+     */
     protected $postalCodeModel;
 
+    /**
+     * GoogleMaps constructor.
+     */
     protected function __construct()
     {
         $this->savePredictions = (bool)config('laurel.google_maps.save_predictions');
@@ -36,6 +99,11 @@ class GoogleMaps
         $this->setPostalCodeModelClass(config('laurel.google_maps.postal_codes.model'));
     }
 
+    /**
+     * Setter for class name of a country model
+     *
+     * @param $className
+     */
     public function setCountryModelClass($className)
     {
         if (!class_exists($className)) {
@@ -44,11 +112,21 @@ class GoogleMaps
         $this->countryModelClass = $className;
     }
 
+    /**
+     * Getter for class name of a country model
+     *
+     * @return mixed
+     */
     public function getCountryModelClass()
     {
         return $this->countryModelClass;
     }
 
+    /**
+     * Setter for class name of a region model
+     *
+     * @param $className
+     */
     public function setRegionModelClass($className)
     {
         if (!class_exists($className)) {
@@ -57,11 +135,21 @@ class GoogleMaps
         $this->regionModelClass = $className;
     }
 
+    /**
+     * Getter for class name of a region model
+     *
+     * @return mixed
+     */
     public function getRegionModelClass()
     {
         return $this->regionModelClass;
     }
 
+    /**
+     * Setter for class name of a city model
+     *
+     * @param $className
+     */
     public function setCityModelClass($className)
     {
         if (!class_exists($className)) {
@@ -70,11 +158,21 @@ class GoogleMaps
         $this->cityModelClass = $className;
     }
 
+    /**
+     * Getter for class name of a city model
+     *
+     * @return mixed
+     */
     public function getCityModelClass()
     {
         return $this->cityModelClass;
     }
 
+    /**
+     * Setter for class name of a postal code model
+     *
+     * @param $className
+     */
     public function setPostalCodeModelClass($className)
     {
         if (!class_exists($className)) {
@@ -83,15 +181,28 @@ class GoogleMaps
         $this->postalCodeModelClass = $className;
     }
 
+    /**
+     * Getter for class name of a postal code model
+     *
+     * @return mixed
+     */
     public function getPostalCodeModelClass()
     {
         return $this->postalCodeModelClass;
     }
 
+    /**
+     * Clone magic method
+     */
     protected function __clone()
     {
     }
 
+    /**
+     * Returns entity instance
+     *
+     * @return GoogleMaps
+     */
     public static function instance()
     {
         if (!self::$instance) {
@@ -101,28 +212,45 @@ class GoogleMaps
         return self::$instance;
     }
 
+    /**
+     * Activates prediction saving
+     */
     public function savePredictions()
     {
         $this->savePredictions = true;
     }
 
+    /**
+     * Deactivates prediction saving
+     */
     public function dontSavePredictions()
     {
         $this->savePredictions = false;
     }
 
+    /**
+     * Returns true, if predictions saving is activated
+     *
+     * @return bool
+     */
     public function doPredictionsSave()
     {
         return $this->savePredictions;
     }
 
+    /**
+     * Method for searching cities, regions, countries and postal codes in GoogleMaps
+     *
+     * @param string $searchQuery
+     * @return array|mixed
+     */
     public function autocomplete(string $searchQuery)
     {
         $response = $this->fetchPredictions($searchQuery);
 
         if (!empty($response['predictions']) && !empty($response['status']) && $response['status'] === "OK") {
             if ($this->doPredictionsSave()) {
-                $this->savePredictionsInDatabase($response['predictions']);
+                return $this->savePredictionsInDatabase($response['predictions']);
             }
             return $response['predictions'];
         } else {
@@ -130,6 +258,12 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Fetches predictions from GoogleMaps API
+     *
+     * @param string $searchQuery
+     * @return mixed
+     */
     public function fetchPredictions(string $searchQuery)
     {
         $route = $this->getAutocompleteRoute();
@@ -143,11 +277,21 @@ class GoogleMaps
         return $this->sendRequest($route, $parameters);
     }
 
+    /**
+     * Returns autocomplete route
+     *
+     * @return string
+     */
     protected function getAutocompleteRoute()
     {
          return config('laurel.google_maps.api_endpoint') . "place/autocomplete/json";
     }
 
+    /**
+     * Returns application locale
+     *
+     * @return string
+     */
     protected function getLocale()
     {
         try {
@@ -157,14 +301,34 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Calls methods for saving predictions in database
+     *
+     * @param array $predictions
+     * @return array
+     */
     protected function savePredictionsInDatabase(array $predictions)
     {
+        $predictionModels = [];
         foreach ($predictions as $index => $prediction) {
-            $this->savePredictionByType($prediction);
+            $this->saveSinglePrediction($prediction);
+
+            $predictionModels[] = [
+                'city' => $this->cityModel,
+                'region' => $this->regionModel,
+                'country' => $this->countryModel,
+                'postal_code' => $this->postalCodeModel,
+            ];
+
             $this->clearModels();
         }
+
+        return $predictionModels;
     }
 
+    /**
+     * Clears filled models
+     */
     protected function clearModels()
     {
         $this->cityModel = null;
@@ -173,7 +337,12 @@ class GoogleMaps
         $this->postalCodeModel = null;
     }
 
-    protected function savePredictionByType(array $prediction)
+    /**
+     * Saves single prediction and their terms
+     *
+     * @param array $prediction
+     */
+    protected function saveSinglePrediction(array $prediction)
     {
         if (empty($prediction['types'])) {
             return;
@@ -191,6 +360,11 @@ class GoogleMaps
         $this->saveModels();
     }
 
+    /**
+     * Detects prediction type and builds model for each of this
+     *
+     * @param array $prediction
+     */
     protected function processPrediction(array $prediction)
     {
         if (in_array('locality', $prediction['types']) && empty($this->cityModel)) {
@@ -204,6 +378,11 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Saves prediction terms
+     *
+     * @param array $prediction
+     */
     protected function processTerms(array $prediction)
     {
         if (!empty($prediction['terms']) && is_array($prediction['terms'])) {
@@ -222,18 +401,30 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Creates city model using prediction
+     *
+     * @param array $prediction
+     * @return mixed
+     */
     protected function buildCityModel(array $prediction)
     {
         $cityClass = $this->getCityModelClass();
         return $cityClass::firstOrNew([
             config('laurel.google_maps.cities.fields.google_id') => $prediction['id']
-        ],[
+        ], [
             config('laurel.google_maps.cities.fields.name') => $prediction['structured_formatting']['main_text'],
             config('laurel.google_maps.cities.fields.slug') => Str::slug($prediction['structured_formatting']['main_text']),
             config('laurel.google_maps.cities.fields.google_id') => $prediction['id']
         ]);
     }
 
+    /**
+     * Creates country model using prediction
+     *
+     * @param array $prediction
+     * @return mixed
+     */
     protected function buildCountryModel(array $prediction)
     {
         $countryClass = $this->getCountryModelClass();
@@ -246,6 +437,12 @@ class GoogleMaps
         ]);
     }
 
+    /**
+     * Creates region model using prediction
+     *
+     * @param array $prediction
+     * @return mixed
+     */
     protected function buildRegionModel(array $prediction)
     {
         $regionClass = $this->getRegionModelClass();
@@ -258,6 +455,12 @@ class GoogleMaps
         ]);
     }
 
+    /**
+     * Creates postal code model using prediction
+     *
+     * @param array $prediction
+     * @return mixed
+     */
     protected function buildPostalCodeModel(array $prediction)
     {
         $postalCodeClass = $this->getPostalCodeModelClass();
@@ -270,6 +473,9 @@ class GoogleMaps
         ]);
     }
 
+    /**
+     * Calls methods for saving models
+     */
     protected function saveModels()
     {
         $this->saveCountryModel();
@@ -278,6 +484,9 @@ class GoogleMaps
         $this->savePostalCodeModel();
     }
 
+    /**
+     * Saves country model, if it exists
+     */
     protected function saveCountryModel()
     {
         if (!empty($this->countryModel)) {
@@ -285,6 +494,9 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Saves region model, if it exists
+     */
     protected function saveRegionModel()
     {
         if (!empty($this->regionModel)) {
@@ -297,6 +509,9 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Saves city model, if it exists
+     */
     protected function saveCityModel()
     {
         if (!empty($this->cityModel)) {
@@ -314,6 +529,9 @@ class GoogleMaps
         }
     }
 
+    /**
+     * Saves postal code model, if it exists
+     */
     protected function savePostalCodeModel()
     {
         if (!empty($this->postalCodeModel)) {
--- config/google_maps.php
+++ config/google_maps.php
@@ -1,10 +1,29 @@
 <?php
     return [
+        /**
+         * GoogleMaps api token
+         */
         'api_token' => 'GOOGLE_MAPS_API_TOKEN',
+
+        /**
+         * GoogleMaps api uri
+         */
         'api_endpoint' => 'https://maps.googleapis.com/maps/api/',
+
+        /**
+         * Need to predictions in database
+         */
         'save_predictions' => true,
+
         'countries' => [
+            /**
+             * Countries model name
+             */
             'model' => 'COUNTRIES_MODEL',
+
+            /**
+             * Field names of countries entity
+             */
             'fields' => [
                 'google_id' => '',
                 'name' => '',
@@ -12,35 +31,68 @@
             ]
         ],
         'regions' => [
+            /**
+             * Regions model name
+             */
             'model' => 'REGIONS_MODEL',
+
+            /**
+             * Field names of regions entity
+             */
             'fields' => [
                 'google_id' => '',
                 'name' => '',
                 'slug' => ''
             ],
+
+            /**
+             * Relations method for regions entity
+             */
             'relations' => [
                 'country_relation_method' => 'country'
             ]
         ],
         'cities' => [
+            /**
+             * Cities model name
+             */
             'model' => 'CITIES_MODEL',
+
+            /**
+             * Field names of cities entity
+             */
             'fields' => [
                 'google_id' => '',
                 'name' => '',
                 'slug' => ''
             ],
+
+            /**
+             * Relations method for cities entity
+             */
             'relations' => [
                 'country_relation_method' => 'country',
                 'region_relation_method' => 'region'
             ]
         ],
         'postal_codes' => [
+            /**
+             * Postal code model name
+             */
             'model' => 'POST_CODES_MODEL',
+
+            /**
+             * Field names of postal codes entity
+             */
             'fields' => [
                 'google_id' => '',
                 'name' => '',
                 'slug' => ''
             ],
+
+            /**
+             * Relations method for postal codes entity
+             */
             'relations' => [
                 'city_relation_method' => 'city',
             ]
